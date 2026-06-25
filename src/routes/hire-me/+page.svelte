d561dd0cad47db16ec81f26dc0b32cfe9864b7b2
Mika <mika@simplymika.com>
2026-06-25 16:46:24 +0200

    Write hire-me page

---
+++ src/routes/hire-me/+page.svelte
@@ -0,0 +1,28 @@
+<h1>I want to be hired!</h1>
+<p>
+	I've not been able to find a development job for quite a while now. I keep getting rejected, likely because my CV has a few
+	pretty obvious gaps in it.
+</p>
+<p>
+	I'm looking for a company that is willing to look past the superficial and be rewarded with an <strong>A+ employee</strong> (me)!
+</p>
+<h2>Are you looking for a dev?</h2>
+<p>
+	I'm more experienced than my CV makes it out to be. I've been programming for over <strong>10 years</strong> as a hobby now!
+</p>
+<p>
+	I'm mostly self-taught, but I did enjoy a 2.5-year apprenticeship at <a href="https://brickmakers.de">BRICKMAKERS</a> in Koblenz,
+	where I learned to focus even more on quality and architecture.
+</p>
+<p>
+	After that, I continued to work there briefly before moving all the way across the country and starting at
+	<a href="https://uniwunder.com">Uniwunder</a> in Dresden.
+</p>
+<p>
+	Unfortunately, Uniwunder and I had very different expectations for my work there, so we mutually parted ways after just 6
+	months.
+</p>
+<p>
+	Nevertheless, during my time there I experienced a very different kind of work atmosphere and learned to work outside of my
+	comfort zone.
+</p>
